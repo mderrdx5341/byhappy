a8507d3dec10811618d55ca8d4e874860f529748
mderrdx5341 <mderrdx5341@gmail.com>
2025-08-27 23:17:42 +0300

    cart action

--- frontend/src/Core/Cart.ts
+++ frontend/src/Core/Cart.ts
@@ -4,6 +4,7 @@ import GameObjects from './GameObjects';
 
 class Cart
 {
+    private _gameObjects: GameObjects;
     private _name: string = 'title';
     private _description: string = 'description';
     private _effects: Effect[];
@@ -11,6 +12,7 @@ class Cart
 
     constructor(gameObject: GameObjects, name: string = 'title', description: string = 'description', type: CartType = CartType.Item)
     {
+        this._gameObjects = gameObject;
         this._name = name;
         this._description = description;
         this._type = type;
@@ -33,7 +35,7 @@ class Cart
 
     public action()
     {
-
+        this._gameObjects.getStep().addStep();
     }
 
     public type()
