b072b3604c8934e2c9fdf2703352ac3be0e28ee8
mderrdx5341 <mderrdx5341@gmail.com>
2025-08-28 19:51:53 +0300

    added time

--- frontend/src/Core/Time.ts
+++ frontend/src/Core/Time.ts
@@ -3,13 +3,13 @@ import Observer from './Observer';
 
 class Time
 {
-    private _step: number;
+    private _time: number;
     private _hours: number;
     private _days: number;
 
     public constructor()
     {
-        this._step = 0;
+        this._time = 8;
         this._hours = 0;
         this._days = 0;
     }
@@ -19,21 +19,35 @@ class Time
         return this._days;
     }
 
-    public addHours(steps: number = 1)
+    public getTime(): number
     {
-        this._step += steps;
-        this._days = Math.floor(this._step / 24);
+        return this._time;
     }
 
-    public subHours(steps: number = 1)
+    public addHours(hours: number = 1)
     {
-        this._step -= steps;
-        this._days = Math.floor(this._step / 24);
+        this._hours += hours;
+        this._days = Math.floor((this._hours + 8)  / 24);
+
+        let t = 8 + this._hours;
+        console.log(24 * this._days)
+        console.log(t);
+        if (t > 24) {
+            this._time = t - (24 * this._days);
+        } else {
+            this._time = t;
+        }
+    }
+
+    public subHours(hours: number = 1)
+    {
+        this._hours -= hours;
+        this._days = Math.floor(this._hours  / 24);
     }
 
     public getHours(): number
     {
-        return this._step;
+        return this._hours;
     }
 }
 
--- frontend/src/Views/StatusLIne/StatusLine.ts
+++ frontend/src/Views/StatusLIne/StatusLine.ts
@@ -3,6 +3,7 @@ import * as Core from '../../Core';
 class StatusLine implements Core.ISubscriber
 {
     private _time: Core.Time;
+    private _timeHtml: HTMLElement;
     private _daysHtml: HTMLElement;
     private _hoursHtml: HTMLElement;
 
@@ -16,6 +17,10 @@ class StatusLine implements Core.ISubscriber
         let statusLine = document.createElement('div');
         statusLine.className = 'status-line';
 
+        this._timeHtml = document.createElement('div');
+        this._timeHtml.className = 'status-line__time';
+        this._timeHtml.innerHTML = 'Time: ' + this._time.getTime();
+
         this._daysHtml = document.createElement('div');
         this._daysHtml.className = 'status-line__days';
         this._daysHtml.innerHTML = 'Days: ' + this._time.getDays();
@@ -24,6 +29,7 @@ class StatusLine implements Core.ISubscriber
         this._hoursHtml.className = 'status-line__hours';
         this._hoursHtml.innerHTML = 'Hours: ' + this._time.getHours();
 
+        statusLine.appendChild(this._timeHtml);
         statusLine.appendChild(this._daysHtml);
         statusLine.appendChild(this._hoursHtml);
 
@@ -32,6 +38,7 @@ class StatusLine implements Core.ISubscriber
 
     public update()
     {
+        this._timeHtml.innerHTML = 'Time: ' + this._time.getTime();
         this._hoursHtml.innerHTML = 'Hours: ' + this._time.getHours();
         this._daysHtml.innerHTML = 'Days: ' + this._time.getDays();
     }
